483091bdbbda89b3e6ca5ef572e081525dc583bb
101ta28 <workta28@gmail.com>
2022-11-20 20:41:57 +0900

    not use GUI

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import json
 import re
 import time
-import urllib
 
-import PySimpleGUI as sg
+import requests
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from seleniumwire import webdriver
@@ -11,44 +10,20 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 
 def main():
-	sg.theme("DarkAmber")
-
-	layout = [
-		[sg.Text("Enter URL")],
-		[sg.Input(key="url", size=(50, 1))],
-		[sg.Text("Enter Save Path"), sg.InputText(), sg.FolderBrowse(key="save_path")],
-		[sg.Button("Download", key="download"),sg.Text("", key="status", size=(50, 1))],
-	]
-
-	window = sg.Window("Box Downloader", layout)
-
-	while True:
-		event, values = window.read()
-		if event in (sg.WIN_CLOSED, "Exit"):
-			break
-		if event == "download":
-			window["status"].update("Downloading...")
-			if values["url"] == "":
-				window["status"].update("URL cannot be empty")
-				continue
-			if values["save_path"] == "":
-				window["status"].update("Save Path cannot be empty")
-				continue
-			if values["url"].startswith("https://app.box.com/s/") == False:
-				window["status"].update("Invalid URL")
-				continue
-			url = values["url"]
-			pdf_url = get_url(url)
-			file_download(pdf_url, values["save_path"] + "/download.pdf")
-	window.close()
-
-
-def file_download(url, save_path):
-	try:
-		with urllib.request.urlopen(url) as box_file, open(save_path, 'wb') as local_file:
-			local_file.write(box_file.read())
-	except urllib.error.URLError as e:
-		print(e)
+	box_url = input("Enter Box URL: ")
+	if box_url == "":
+		print("URL cannot be empty")
+	if box_url.startswith("https://app.box.com/s/") == False:
+		print("Invalid URL")
+	pdf_url = get_url(box_url)
+	data = requests.get(pdf_url, stream=True)
+	if data.status_code == 200:
+		with open("file.pdf", "wb") as f:
+			for chunk in data.iter_content(1024):
+				f.write(chunk)
+		print("File saved as file.pdf")
+	else:
+		print("Error downloading file")
 
 
 def get_url(box_url):
